9a5110cabb50b7891055f91b0b90fe368e4c3b35
JeonYeon-Hwang <ubitnoa@gmail.com>
2026-05-27 00:15:18 +0900

    feat: bpe .. train 까지 구현

--- src/bpe.py
+++ src/bpe.py
@@ -39,11 +39,23 @@ class BPETokenizer:
 
     def _init_special_tokens(self):
         """
-        TODO:
         1. 특수 토큰 4개를 고정 ID 0~3에 등록합니다.
         2. byte 0~255를 ID 4~259에 bytes([byte_value]) 형태로 등록합니다.
         """
-        raise NotImplementedError("_init_special_tokens를 구현하세요.")
+
+        # 특수 토큰을 일단 dict에 넣는다
+        for i in range(len(SPECIAL_TOKENS)):
+            target_token = SPECIAL_TOKENS[i]
+            self.id_to_token[i] = target_token
+            self.token_to_id[target_token] = i
+
+        # 나머지 259까지는 일반 토큰이다
+        # 255개의 낱글자이다: 이후 조합으로 단어 생성됨
+        for i in range(len(SPECIAL_TOKENS), 260):
+            byte_cnt = i - 4
+            self.id_to_token[i] = bytes([byte_cnt])
+            self.token_to_id[bytes([byte_cnt])] = i
+
 
     def get_pad_id(self):
         """padding 토큰 ID."""
@@ -61,17 +73,64 @@ class BPETokenizer:
         """문장 끝 토큰 ID."""
         return SPECIAL_IDS[EOS_TOKEN]
 
-    def train(self, corpus: str):
+    def train(self, corpus: str):   # corpus: 통째로 온 긴 원본 문장
         """
-        TODO: 코퍼스에서 BPE merge rule과 vocabulary를 학습합니다.
-
         구현 힌트:
         - `corpus.encode("utf-8")`로 byte ID 시퀀스를 만듭니다.
         - 가장 자주 등장하는 이웃 token pair를 찾습니다.
         - 새 token ID를 만들고, 시퀀스의 해당 pair를 새 ID로 치환합니다.
         - `self.merges`, `self.id_to_token`, `self.token_to_id`를 갱신합니다.
         """
-        raise NotImplementedError("BPETokenizer.train을 구현하세요.")
+        # 초기화 진행
+        if not self.id_to_token:
+            self._init_special_tokens()
+
+        # 초기 셋팅
+        encoded_corpus = corpus.encode("utf-8")
+        sequence = [b + 4 for b in encoded_corpus]
+        
+        # 1. 반복문으로 단어 쌍(token pair)를 생성한다
+        while(len(self.id_to_token) < self.vocab_size):
+            counts = {}
+            # 2. 매 루프 마다 최대 반복 pair 갱신
+            for i in range(1, len(sequence)):
+                pair = (sequence[i - 1], sequence[i])
+                counts[pair] = counts.get(pair, 0) + 1
+
+            # 탈출조건: sequence를 다 소진했을 경우
+            if not counts:
+                break
+
+            # 3. 최대 반복 pair 찾기 & 바이트로 변환
+            max_pair = max(counts, key=counts.get)
+            token1 = self.id_to_token[max_pair[0]]
+            token2 = self.id_to_token[max_pair[1]]
+
+            # 4. 토큰 자료구조 갱신
+            new_id = len(self.id_to_token)
+            self.id_to_token[new_id] = token1 + token2
+            self.token_to_id[token1 + token2] = new_id
+            self.merges.append((max_pair, new_id))
+
+            # 5. sequence 갱신
+            new_sequence = []
+            n = 0
+            while n < len(sequence) - 1:
+                if max_pair[0] == sequence[n] and max_pair[1] == sequence[n + 1]: 
+                    new_sequence.append(new_id)
+                    n += 2
+                else:
+                    new_sequence.append(sequence[n])
+                    n += 1
+
+            # 6. 예외 상황 보완
+            if n == len(sequence) - 1:
+                new_sequence.append(sequence[n])
+
+            sequence = new_sequence
+                
+
+        
 
     def save(self, path: str | Path):
         """
